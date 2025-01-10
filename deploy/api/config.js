17f77df63e1a6fd1a9fdfc426d417654a9cc3140
Skullkrash <jeannotnaegely@gmail.com>
2025-01-10 09:46:50 +0100

    updated config.js to link to my render database

--- deploy/api/config.js
+++ deploy/api/config.js
@@ -1,10 +1,10 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-cok0d7gl6cac73e70b2g-a.oregon-postgres.render.com",
+    "host" :"dpg-cu0dgvdds78s73dc2isg-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_l5ol_user",
-    "password" : "VoZBXopqdGnJ5Vilgny4Jc0ANIK6XqPY",
-    "bdname" :"cnam_l5ol" 
+    "user" : "cnam_83cz_user",
+    "password" : "q8coO1tzZsmqA6b2N5PZ5BeupSKuMRu4",
+    "bdname" :"cnam_83cz" 
     }
 }
